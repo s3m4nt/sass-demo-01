5faf6caec43ea814d9f7045cd998d765012c34de
Brian Young <berkshirehunt@gmail.com>
2021-07-30 12:37:31 -0700

    configs + funcs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 <header class="showcase">
 			<div class="container">
 				<nav>
-					<h1 class="logo">My Website</h1>
+					<h1 class="logo">SASS-styled website</h1>
 					<ul>
 						<li><a href="#">Home</a></li>
 						<li><a href="#">About</a></li>
@@ -17,11 +17,9 @@ function App() {
 
 				<div class="showcase-content">
 					<div>
-						<h1>Make Your Marketing Real</h1>
+						<h1>Hello, Syntactically Awesome Style Sheets!</h1>
 						<p class="my-1">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
-							eligendi tempore atque laborum. Quisquam nemo at non. Corrupti,
-							vitae dolore.
+						Sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets. SassScript is the scripting language itself. Sass consists of two syntaxes. The original syntax, called "the indented syntax," uses a syntax similar to Haml &mdash; Wikipedia
 						</p>
 						<a href="#" class="btn-primary">Learn More</a>
 						<a href="#" class="btn-secondary">Sign Up</a>
